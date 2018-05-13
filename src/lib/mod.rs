e44d670e57e7a3269d79652a02896ead1f8825fe
Dathan Ault-McCoy <kwarrtz@icloud.com>
2018-05-13 11:21:46 -0700

    Added variations, other minor improvements.

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,46 +1,19 @@
 use std::thread;
 use rand::thread_rng;
 use rand::distributions::{Range, IndependentSample};
-use nalgebra::{Transform,Matrix3,Affine2, Point2};
+use nalgebra::{Transform,Matrix3,Point2,Affine2};
 use std::fs::File;
 use serde_json;
+use std::f32::consts::PI;
 
-pub mod cli;
+const PII: f32 = 1.0 / PI;
 
 #[cfg(test)]
 mod test;
 
-#[derive(Deserialize)]
-struct FlameSource {
-    bounds: [f32;4],
-    transformations: Vec<[f32;7]>,
-}
-
-impl FlameSource {
-    fn from_file(f : File) -> serde_json::Result<FlameSource> {
-        serde_json::from_reader(f)
-    }
-
-    fn to_flame(self) -> Flame {
-        let trans = self.transformations.iter().map(|t| {
-            let a = Transform::from_matrix_unchecked(Matrix3::new(
-                t[1], t[2], t[5],
-                t[3], t[4], t[6],
-                0.,   0.,    1.));
-            (t[0],a)
-        }).collect();
-
-        Flame {
-            x_min: self.bounds[0], x_max: self.bounds[1],
-            y_min: self.bounds[2], y_max: self.bounds[3],
-            transformations: trans,
-        }
-    }
-}
-
 #[derive(Clone)]
 pub struct Flame {
-    pub transformations: Vec<(f32,Affine2<f32>)>,
+    pub functions: Vec<(f32,Function)>,
     pub x_min: f32,
     pub x_max: f32,
     pub y_min: f32,
@@ -61,23 +34,8 @@ pub struct Config {
     pub workers: usize,
 }
 
-type Counter = Vec<Vec<u32>>;
-
-fn choose_trans(trans: &Vec<(f32,Affine2<f32>)>) -> Affine2<f32> {
-    let r = Range::new(0.0,1.0).ind_sample(&mut thread_rng());
-    let mut x = 0.0;
-    for &(p,t) in trans {
-        x += p;
-        if r < x {
-            return t;
-        }
-    }
-
-    trans.iter().last().unwrap().1
-}
-
 pub fn run_single(f: Flame, c: Config) -> Counter {
-    let mut counter = vec![vec![0;c.width];c.height];
+    let mut counter = vec![vec![Bucket::new();c.width];c.height];
 
     let range = Range::new(0.0,1.0);
     let mut rng = thread_rng();
@@ -86,13 +44,13 @@ pub fn run_single(f: Flame, c: Config) -> Counter {
                                , range.ind_sample(&mut rng));
 
     for _ in 0..(c.iterations / c.workers) {
-        point = choose_trans(&f.transformations) * point;
+        point = choose_func(&f.functions).eval(point);
         let mut x = point[0];
         let mut y = point[1];
         if x > f.x_min && x < f.x_max && y > f.y_min && y < f.y_max {
             x = (x - f.x_min) * (c.width - 1) as f32 / (f.x_max - f.x_min);
             y = (y - f.y_min) * (c.height - 1) as f32 / (f.y_max - f.y_min);
-            counter[y as usize][x as usize] += 1;
+            counter[y as usize][x as usize].plot();
         }
     }
 
@@ -104,9 +62,9 @@ pub fn to_buffer(buckets: Counter) -> Vec<u8> {
     let mut max = 0.0;
     for row in buckets.iter().rev() {
         for &bucket in row {
-            let val = (bucket as f32).ln();
-            buf.push(val);
+            let val = (bucket.alpha as f32).ln();
             max = f32::max(max,val);
+            buf.push(val);
         }
     }
 
@@ -116,7 +74,7 @@ pub fn to_buffer(buckets: Counter) -> Vec<u8> {
 
 pub fn run(flame: Flame, c: Config) -> Vec<u8> {
     let mut handles = Vec::new();
-    let mut buckets = vec![vec![0;c.width];c.height];
+    let mut buckets = vec![vec![Bucket::new();c.width];c.height];
 
     for _ in 0..c.workers {
         let f = flame.clone();
@@ -126,11 +84,202 @@ pub fn run(flame: Flame, c: Config) -> Vec<u8> {
     for handle in handles {
         let counter = handle.join().unwrap();
         for (i,row) in counter.iter().enumerate() {
-            for (j,count) in row.iter().enumerate() {
-                buckets[i][j] += count;
+            for (j,bucket) in row.iter().enumerate() {
+                buckets[i][j] += *bucket;
             }
         }
     }
 
     to_buffer(buckets)
 }
+
+fn choose_func(funcs: &Vec<(f32,Function)>) -> Function {
+    let r = Range::new(0.0,1.0).ind_sample(&mut thread_rng());
+    let mut x = 0.0;
+    for &(p,t) in funcs {
+        x += p;
+        if r < x {
+            return t;
+        }
+    }
+
+    funcs.iter().last().unwrap().1
+}
+
+#[allow(unused)]
+fn sample_palette(p: Palette, i: f32) -> Color {
+    if i >= 0.0 && i <= 1.0 {
+        p[(i * 255.0) as usize]
+    } else {
+        panic!("Palette sample index must be between 0 and 1")
+    }
+}
+
+#[derive(Deserialize)]
+struct FlameSource {
+    bounds: [f32;4],
+    functions: Vec<FunctionSource>,
+}
+
+impl FlameSource {
+    fn from_file(f : File) -> serde_json::Result<FlameSource> {
+        serde_json::from_reader(f)
+    }
+
+    fn to_flame(self) -> Flame {
+        let funcs = self.functions.iter().map(FunctionSource::to_function).collect();
+
+        Flame {
+            x_min: self.bounds[0], x_max: self.bounds[1],
+            y_min: self.bounds[2], y_max: self.bounds[3],
+            functions: funcs,
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct FunctionSource(f32,Variation,[f32;6]);
+
+impl FunctionSource {
+    fn to_function(&self) -> (f32,Function) {
+        let t = Transform::from_matrix_unchecked(Matrix3::new(
+            self.2[0], self.2[1], self.2[4],
+            self.2[2], self.2[3], self.2[5],
+            0.0,       0.0,       1.0
+        ));
+
+        let f = Function {
+            var: self.1,
+            trans: t,
+        };
+
+        (self.0, f)
+    }
+}
+
+#[derive(Copy,Clone)]
+pub struct Function {
+    pub var: Variation,
+    pub trans: Affine2<f32>,
+}
+
+impl Function {
+    pub fn eval(&self, arg: Point2<f32>) -> Point2<f32> {
+        self.var.eval(self.trans * arg)
+    }
+}
+
+#[derive(Clone,Copy,Deserialize)]
+pub enum Variation {
+    Id, Sinusoidal, Spherical, Swirl, Horseshoe, Polar,
+    Handkerchief, Heart, Disc, Spiral, Hyperbolic,
+    Diamond, Ex, Bent, Fisheye, Eyefish, Exponential,
+    Cylinder, Tangent, Blob(f32,f32,f32), PDJ(f32,f32,f32,f32),
+}
+
+impl Variation {
+    pub fn eval(self, arg: Point2<f32>) -> Point2<f32> {
+        use self::Variation::*;
+
+        let (x,y) = (arg[0], arg[1]);
+        let r = x.powi(2) + y.powi(2);
+        let theta = if y == 0.0 {
+            if x == 0.0 {
+                0.0
+            } else if x > 0.0 {
+                0.5 * PI
+            } else {
+                1.5 * PI
+            }
+        } else {
+            (x/y).atan()
+        };
+
+        let (xo,yo) = match self {
+            Id => (x,y),
+            Sinusoidal => (x.sin(),y.sin()),
+            Spherical => (x/r, y/r),
+            Swirl => (x * r.sin() - y * r.cos(), x * r.cos() + y * r.sin()),
+            Horseshoe => ((x-y)*(x+y)/r, 2.0*x*y/r),
+            Polar => (theta * PII, r - 1.0),
+            Handkerchief => ((theta + r).sin(), (theta - r).cos()),
+            Heart => (r * (theta * r).sin(), -r * (theta * r).cos()),
+            Disc => (theta * PII * (PI * r).sin(), theta * PII),
+            Spiral => ((theta.cos() + r.sin())/r, (theta.sin() - r.cos())/r),
+            Hyperbolic => (theta.sin() / r, r * theta.cos()),
+            Diamond => (theta.sin() * r.cos(), theta.cos() * r.sin()),
+            Ex => {
+                let p0 = (theta + r).sin().powi(3);
+                let p1 = (theta - r).cos().powi(3);
+                (r * (p0 + p1), r * (p0 - p1))
+            },
+            Bent => {
+                let a = if x >= 0.0 { x } else { 2.0 * x };
+                let b = if y >= 0.0 { y } else { 0.5 * y };
+                (a,b)
+            },
+            Fisheye => (2.0 * y / (r + 1.0), 2.0 * x / (r + 1.0)),
+            Eyefish => (2.0 * x / (r + 1.0), 2.0 * y / (r + 1.0)),
+            Exponential => ((x - 1.0).exp() * (PI * y).cos(), (x - 1.0).exp() * (PI * y).sin()),
+            Cylinder => (x.sin(), y),
+            Tangent => (x.sin() / y.cos(), y.tan()),
+            Blob(h,l,w) => {
+                let a = r * (l + (h - l)/2.0 * (1.0 + (theta * w).sin()));
+                (a * theta.cos(), a * theta.sin())
+            },
+            PDJ(a,b,c,d) => ((a*y).sin() - (b*x).cos(), (c*x).sin() - (d*y).cos())
+        };
+
+        Point2::new(xo,yo)
+    }
+}
+
+#[derive(Clone,Copy)]
+pub struct Bucket {
+    pub alpha: u64
+}
+
+impl Bucket {
+    pub fn new() -> Self {
+        Bucket {
+            alpha: 0
+        }
+    }
+
+    pub fn plot(&mut self) {
+        self.alpha += 1;
+    }
+}
+
+impl ::std::ops::Add<Bucket> for Bucket {
+    type Output = Bucket;
+
+    fn add(self, rhs: Bucket) -> Bucket {
+        Bucket { alpha: self.alpha + rhs.alpha }
+    }
+}
+
+impl ::std::ops::AddAssign<Bucket> for Bucket {
+    fn add_assign(&mut self, rhs: Bucket) {
+        self.alpha += rhs.alpha;
+    }
+}
+
+pub type Counter = Vec<Vec<Bucket>>;
+
+#[allow(unused)]
+#[derive(Clone,Copy)]
+pub struct Color {
+    red: u8,
+    green: u8,
+    blue: u8
+}
+
+#[allow(unused)]
+impl Color {
+    pub fn from_rgb(red: u8, green: u8, blue: u8) -> Self {
+        Color { red, green, blue }
+    }
+}
+
+pub type Palette = [Color;256];
--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,58 @@ extern crate ansi_term;
 use image::ColorType;
 use std::fs::File;
 use ansi_term::Color::Red;
+use clap::Arg;
 
 mod lib;
 use lib::*;
 
 fn main() {
-    let matches = cli::make_app().get_matches();
+    let app = app_from_crate!()
+        .arg(Arg::with_name("dimensions")
+            .long("dims")
+            .short("d")
+            .help("Dimensions (in pixels) of the output image")
+            .takes_value(true)
+            .multiple(true)
+            .require_delimiter(true)
+            .number_of_values(2)
+            .value_names(&["WIDTH","HEIGHT"])
+            .default_value("500,500"))
+        .arg(Arg::with_name("iterations")
+            .value_name("N")
+            .long("iters")
+            .short("i")
+            .help("Number of iterations of the chaos game to run")
+            .takes_value(true)
+            .default_value("8"))
+        .arg(Arg::with_name("jobs")
+            .value_name("N")
+            .long("jobs")
+            .short("j")
+            .help("Number of parallel jobs")
+            .takes_value(true)
+            .default_value("4"))
+        .arg(Arg::with_name("INPUT")
+            // .help("Path to flame file to be compiled"
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("OUTPUT")
+            .help("Path to output file (must have .PNG or .JPG extension)")
+            .required(true)
+            .index(2)
+            .validator(check_output));
+
+    let matches = app.get_matches();
 
     let dims = values_t_or_exit!(matches,"dimensions",usize);
-    let iters = value_t_or_exit!(matches,"iterations",usize);
+    let iters = value_t_or_exit!(matches,"iterations",u32);
     let workers = value_t_or_exit!(matches,"jobs",usize);
     let path = matches.value_of("OUTPUT").unwrap();
     let input_file = or_exit(File::open(matches.value_of("INPUT").unwrap()));
 
     let conf = Config {
         width: dims[0], height: dims[1],
-        iterations: iters,
+        iterations: usize::pow(10, iters),
         workers: workers,
     };
 
@@ -37,7 +73,11 @@ fn main() {
 
     println!("Compiling flame...");
 
+    let before_run = std::time::Instant::now();
+
     let buf = run(flame, conf);
+
+    let dur = before_run.elapsed();
 
     match image::save_buffer(
         path,
@@ -46,7 +86,11 @@ fn main() {
         conf.height as u32,
         ColorType::Gray(8)
     ) {
-        Ok(()) => println!("Completed! Output written to '{}'", path),
+        Ok(()) => println!(
+            "Completed! Process took {}.{:03} seconds. Output written to '{}'",
+            dur.as_secs(),
+            dur.subsec_millis(),
+            path),
         Err(e) => eprintln!("Failed to write output: {}", e)
     };
 }
@@ -56,7 +100,15 @@ fn or_exit<A,E>(x: Result<A,E>) -> A where E: std::fmt::Display {
         Ok(a) => a,
         Err(e) => {
             eprintln!("{} {}", Red.bold().paint("error:"), e);
-            ::std::process::exit(1)
+            std::process::exit(1)
         }
+    }
+}
+
+fn check_output(path: String) -> Result<(),String> {
+    if path.ends_with(".png") || path.ends_with(".jpg") || path.ends_with(".jpeg") {
+        Ok(())
+    } else {
+        Err(String::from("the output file must have PNG or JPEG format"))
     }
 }
