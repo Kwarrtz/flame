e0800c295b263abd3ead4084ddc8e0804b8de27d
Kwarrtz <dsam10@icloud.com>
2026-03-29 04:17:45 -0700

    cleanup macro code

--- flame_macro/src/lib.rs
+++ flame_macro/src/lib.rs
@@ -23,32 +23,31 @@ pub fn variation(_argument: proc_macro::TokenStream, input: proc_macro::TokenStr
     let dynamic_ident = format_ident!("Dynamic{}", ident);
 
     let variant_idents: Vec<_> = input.variants.iter().map(|v| v.ident.clone()).collect();
-    // let variant_args: Vec<_> = input.variants.iter().map(|v| v.fields.len()).collect();
-    let mut variant_args: Vec<(usize,usize)> = vec![];
-    let mut variant_int_arg_ranges: Vec<Vec<Option<TokenStream>>> = vec![];
-    let mut variant_float_arg_ranges: Vec<Vec<Option<TokenStream>>> = vec![];
+    let mut variant_param_counts: Vec<(usize, usize)> = vec![];
+    let mut variant_int_ranges: Vec<Vec<Option<TokenStream>>> = vec![];
+    let mut variant_float_ranges: Vec<Vec<Option<TokenStream>>> = vec![];
 
     for var in input.variants.clone() {
-        let mut int_args = 0usize;
-        let mut float_args = 0usize;
-        let mut int_arg_ranges = vec![];
-        let mut float_arg_ranges = vec![];
+        let mut int_count = 0usize;
+        let mut float_count = 0usize;
+        let mut int_ranges = vec![];
+        let mut float_ranges = vec![];
 
         for field in var.fields {
             if let Type::Path(TypePath { path, .. }) = field.ty {
                 if path.is_ident("i8") {
-                    int_args += 1;
-                    int_arg_ranges.push(extract_range(&field.attrs[..]));
+                    int_count += 1;
+                    int_ranges.push(extract_range(&field.attrs[..]));
                 } else if path.is_ident("f32") {
-                    float_args += 1;
-                    float_arg_ranges.push(extract_range(&field.attrs[..]));
+                    float_count += 1;
+                    float_ranges.push(extract_range(&field.attrs[..]));
                 }
             }
         }
 
-        variant_args.push((int_args, float_args));
-        variant_int_arg_ranges.push(int_arg_ranges);
-        variant_float_arg_ranges.push(float_arg_ranges);
+        variant_param_counts.push((int_count, float_count));
+        variant_int_ranges.push(int_ranges);
+        variant_float_ranges.push(float_ranges);
     }
 
     let num_variants = variant_idents.len();
@@ -69,22 +68,7 @@ pub fn variation(_argument: proc_macro::TokenStream, input: proc_macro::TokenStr
         }
     };
 
-    // macro_rules! fields {
-    //     ($f:expr) => {
-    //         variant_args.iter().map(|&n| {
-    //             if n == 0 {
-    //                 quote! {}
-    //             } else {
-    //                 let param = (0..n).map($f);
-    //                 quote! { (#(#param),*) }
-    //             }
-    //         })
-    //     };
-    // }
-
-    // let blank_fields = fields!(|_| quote! {_});
-    
-    let blank_fields = variant_args.iter().map(|(ni, nf)| {
+    let blank_fields = variant_param_counts.iter().map(|(ni, nf)| {
         let n = ni + nf;
         if n == 0 {
             quote! {}
@@ -104,99 +88,44 @@ pub fn variation(_argument: proc_macro::TokenStream, input: proc_macro::TokenStr
         }
     };
 
-    let num_parameters_rhs = variant_args.iter()
+    let num_params_arms = variant_param_counts.iter()
         .map(|(i, f)| quote!{ (#i, #f) });
-    
+
     let num_parameters = quote! {
         pub fn num_parameters(&self) -> (usize, usize) {
             match self {
-                #( &Self::#variant_idents => #num_parameters_rhs ),*
+                #( &Self::#variant_idents => #num_params_arms ),*
             }
         }
     };
 
-    let parameter_ranges_rhs: Vec<TokenStream> =
-        variant_int_arg_ranges.iter()
-        .zip(variant_float_arg_ranges.iter())
-        .map(|(vi, vf)| {
-            let opt_trans = |x: &Option<TokenStream>| {
+    let param_ranges_arms: Vec<TokenStream> =
+        variant_int_ranges.iter()
+        .zip(variant_float_ranges.iter())
+        .map(|(int_ranges, float_ranges)| {
+            let to_option_token = |x: &Option<TokenStream>| {
                 match x.clone() {
                     Some(v) => quote!{ Some(#v) },
                     None => quote! { None }
                 }
             };
 
-            let vi = vi.iter().map(opt_trans);
-            let vf = vf.iter().map(opt_trans);
+            let int_range_tokens = int_ranges.iter().map(to_option_token);
+            let float_range_tokens = float_ranges.iter().map(to_option_token);
 
-            quote! { (vec![#(#vi),*], vec![#(#vf),*]) }
+            quote! { (vec![#(#int_range_tokens),*], vec![#(#float_range_tokens),*]) }
         })
         .collect();
-    
+
     let parameter_ranges = quote! {
         pub fn parameter_ranges(&self) -> (Vec<Option<std::ops::Range<i8>>>, Vec<Option<std::ops::Range<f32>>>) {
             match self {
-                #( &Self::#variant_idents => #parameter_ranges_rhs ),*
+                #( &Self::#variant_idents => #param_ranges_arms ),*
             }
         }
     };
 
-    // let build_fields = fields!(|i| quote! { parameters[#i] });
-    // let build = quote! {
-    //     pub fn build(discr: #discr_ident, parameters: impl ::std::iter::IntoIterator<Item=f32>) -> Option<Self> {
-    //         let parameters: Vec<_> = parameters.into_iter().collect();
-
-    //         if parameters.len() != discr.num_parameters() { return None; }
-
-    //         Some(match discr {
-    //             #(#discr_ident::#variant_idents => Self::#variant_idents #build_fields),*
-    //         })
-    //     }
-    // };
-
-    // let build_fields = fields!(|_| quote! { parameters.next()? });
-
-    // let build = quote! {
-    //     pub fn build(discr: #discr_ident, parameters: impl ::std::iter::IntoIterator<Item=f32>) -> Option<Self> {
-    //         let mut parameters = parameters.into_iter();
-
-    //         let var = match discr {
-    //             #(#discr_ident::#variant_idents => Self::#variant_idents #build_fields),*
-    //         };
-
-    //         match parameters.next() {
-    //             None => Some(var),
-    //             _ => None
-    //         }
-    //     }
-    // };
-
-    // let deconstruct_patterns = variant_args.iter()
-    //     .map(|&n| {
-    //         if n == 0 {
-    //             quote! {}
-    //         } else {
-    //             let params = (0..n).map(|i| format_ident!("p{}", i));
-    //             quote! { (#(#params),*) }
-    //         }
-    //     });
-
-    // let deconstruct_fields = fields!(|i| format_ident!("p{}", i));
-
-    // let deconstruct_vecs = variant_args.iter().map(|&n| {
-    //     let params = (0..n).map(|i| format_ident!("p{}", i));
-    //     quote! { vec![#(#params),*] }
-    // });
-
-    // let deconstruct = quote! {
-    //     pub fn deconstruct(self) -> (#discr_ident, Vec<f32>) {
-    //         match self {
-    //             #( #ident::#variant_idents #deconstruct_fields => (#discr_ident::#variant_idents, #deconstruct_vecs) ),*
-    //         }
-    //     }
-    // };
-    
-    let orig_to_dynamic_pattern = variant_args.iter()
+    let to_dynamic_patterns = variant_param_counts.iter()
         .map(|(ni, nf)| {
             if ni + nf == 0 {
                 return quote!{};
@@ -207,28 +136,28 @@ pub fn variation(_argument: proc_macro::TokenStream, input: proc_macro::TokenStr
             quote!{ (#(#ints,)*#(#floats),*) }
         });
 
-    let orig_to_dynamic_int_vecs = variant_args.iter()
+    let to_dynamic_int_vecs = variant_param_counts.iter()
         .map(|(ni, _)| {
             let params = (0..*ni).map(|idx| format_ident!("i{}", idx));
             quote!{ vec![#(#params),*] }
         });
 
-    let orig_to_dynamic_float_vecs = variant_args.iter()
+    let to_dynamic_float_vecs = variant_param_counts.iter()
         .map(|(_, nf)| {
             let params = (0..*nf).map(|idx| format_ident!("f{}", idx));
             quote!{ vec![#(#params),*] }
         });
 
-    let orig_to_dynamic = quote! {
+    let into_dynamic_impl = quote! {
         impl From<#ident> for #dynamic_ident {
             fn from(val: #ident) -> Self {
                 match val {
                     #(
-                        #ident::#variant_idents #orig_to_dynamic_pattern =>
+                        #ident::#variant_idents #to_dynamic_patterns =>
                         #dynamic_ident {
                             discriminant: #discr_ident::#variant_idents,
-                            int_params: #orig_to_dynamic_int_vecs,
-                            float_params: #orig_to_dynamic_float_vecs,
+                            int_params: #to_dynamic_int_vecs,
+                            float_params: #to_dynamic_float_vecs,
                         }
                     ),*
                 }
@@ -236,7 +165,7 @@ pub fn variation(_argument: proc_macro::TokenStream, input: proc_macro::TokenStr
         }
     };
 
-    let dynamic_to_orig_fields = variant_args.iter()
+    let from_dynamic_fields = variant_param_counts.iter()
         .map(|(ni, nf)| {
             if ni + nf == 0 {
                 return quote!{};
@@ -247,7 +176,7 @@ pub fn variation(_argument: proc_macro::TokenStream, input: proc_macro::TokenStr
             quote!{ (#(#ints,)*#(#floats),*) }
         });
 
-    let dynamic_to_orig = quote! {
+    let from_dynamic_impl = quote! {
         impl TryFrom<#dynamic_ident> for #ident {
             type Error = ();
 
@@ -255,11 +184,11 @@ pub fn variation(_argument: proc_macro::TokenStream, input: proc_macro::TokenStr
                 Ok(match val.discriminant {
                     #(
                         #discr_ident::#variant_idents =>
-                        #ident::#variant_idents #dynamic_to_orig_fields
+                        #ident::#variant_idents #from_dynamic_fields
                     ),*
                 })
             }
-        } 
+        }
     };
 
     let const_discrs_ident = Ident::new(
@@ -269,18 +198,6 @@ pub fn variation(_argument: proc_macro::TokenStream, input: proc_macro::TokenStr
     let const_discrs = quote! {
         pub const #const_discrs_ident: [#discr_ident; #num_variants] = [#(#discr_ident::#variant_idents),*];
     };
-
-    // let match_branches = (0..num_variants);
-    // let rand_impl = quote! {
-    //     impl ::rand::distr::Distribution<#discr_ident> for #discr_ident {
-    //         fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Self {
-    //             match rng.random_range(0..#num_variants) {
-    //                 #( #match_branches => Self::#variant_idents ),*,
-    //                 _ => unreachable!()
-    //             }
-    //         }
-    //     }
-    // };
 
     // strip #[range(...)] attrs from the output enum so the compiler
     // doesn't see unknown attributes on struct fields
@@ -302,17 +219,9 @@ pub fn variation(_argument: proc_macro::TokenStream, input: proc_macro::TokenStr
 
         #from_impl
 
-        // #rand_impl
+        #into_dynamic_impl
 
-        // impl #ident {
-        //     #build
-
-        //     #deconstruct
-        // }
-
-        #orig_to_dynamic
-
-        #dynamic_to_orig
+        #from_dynamic_impl
 
         impl #discr_ident {
             #num_parameters
